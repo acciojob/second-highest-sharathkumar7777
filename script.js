882a4057eec660cbd022cb600e861396491d8c21
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-24 12:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 function secondHighest(arr) {
     if (arr.length < 2) return -Infinity;
 
@@ -16,4 +15,3 @@ function secondHighest(arr) {
 
     return secondMax;
 }
-
